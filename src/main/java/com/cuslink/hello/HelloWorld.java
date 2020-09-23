e5731e858e1acf17ca40efa9030aadfe3c38f18d
ZhangMH12 <757279760@qq.com>
2020-09-23 12:51:46 +0800

    first

---
+++ src/main/java/com/cuslink/hello/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.cuslink.hello;
+
+/**
+ * @author sunqiang
+ * @date 2020-09-04 13:46:38
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
